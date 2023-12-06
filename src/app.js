f0debb505c7041e79a8b2ea2bae1bf2a0eab886c
AlanVT <alanvitas27@gmail.com>
2023-12-06 00:54:57 -0300

    port

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ import path from 'path';
 import __dirname from './utils/utils.js' 
 
 const app = express();
+const port = process.env.PORT || 3000
 
 app.engine('hbs', expressHandlebars.engine({
     extname: 'hbs', // Extensión de archivos Handlebars
@@ -78,4 +79,4 @@ app.get('/health', (req, res) => {
     res.status(200).send('OK');
   });
 
-app.listen(8080, () => console.log('Listening'));
+app.listen(port, () => console.log('Listening'));
